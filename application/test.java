7ce95f5dd56041c6569d7deed60611dab0f2ab58
oumaymahammami <oumayma.hammami@ensi-uma.tn>
2025-10-03 11:29:38 +0100

    second commit

--- application/test.java
+++ application/test.java
@@ -1,20 +1,10 @@
 package application;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
 
 public class test {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        JFrame frame = new JFrame();
-        JButton  ok = new JButton();
-        JButton  ok1 = new JButton("ok1");
-        ok.setText("ok") ; 
-        frame.add(ok) ;
-        frame.setVisible(true);
-        frame.add(ok1);
-        frame.setVisible(true);
+        System.out.println("Hello TPGIT!");
     }
 
 }
