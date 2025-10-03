8ee079f8762c765fe79c2cd3e5baad1e4aa65358
oumaymahammami <oumayma.hammami@ensi-uma.tn>
2025-10-03 11:47:45 +0100

    third commit

--- application/test.java
+++ application/test.java
@@ -4,7 +4,7 @@ package application;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("Hello TPGIT!");
+        System.out.println("Hello Master");
     }
 
 }
